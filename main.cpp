5894613653b67e3ef574d00237b53619cd5e3d7f
sborkovoy <126052001+sborkovoy@users.noreply.github.com>
2024-03-22 18:40:58 +0300

    second version
    Changed distance calculation method. Added '%n' everywhere I could. Seems to show some results.

--- main.cpp
+++ main.cpp
@@ -18,9 +18,6 @@ public:
         this->x = 0;
         this->y = 0;
     }
-    double dist(const Point &other){
-        return sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
-    }
 };
 
 class Solver{
@@ -28,8 +25,11 @@ public:
     double path_dist(vector<Point> &path);
     void do2opt(vector<Point> &path, int i, int j);
     double optimise_path(vector<Point> path);
-    double lengthDelta(vector<Point> &path, int i, int j){
-        return path[j].dist(path[i]) - path[i].dist(path[j]);
+    double dist(Point p1, Point p2){
+        return sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+    }
+    double lengthDelta(Point a1, Point a2, Point b1, Point b2){
+        return -dist(a1, a2)-dist(b1, b2)+dist(a1, b1)+dist(a2, b2);
     }
 };
 
@@ -44,12 +44,11 @@ double Solver::optimise_path(vector<Point> path) {
         can_imrove = false;
         for(int i = 0; i < n - 1; i++){
             for(int j = i + 1; j <= n; j++){
-                double lengthDelta = -path[i].dist(path[(i + 1) % n]) - path[j % n].dist(path[(j + 1) % n]) +
-                        path[i].dist(path[j % n]) + path[(i + 1) % n].dist(path[(j + 1) % n]);
-                //cout << " ---" << lengthDelta << endl;
-                if((lengthDelta) < -EPS){
+                //cout << " ---" << lengthDelta(path[i], path[i+1], path[j], path[(j+1) % n]) << endl;
+                //cout<<i<<" "<<j<<" "<<endl;
+                if((lengthDelta(path[i], path[i+1], path[j], path[j+1])) < -EPS){
                     //cout << "---" << "We are here!" << endl;
-                    do2opt(path, i, j % n);
+                    swap(path[(i+1) % n], path[(j+1) % n]);
                     can_imrove = true;
                 }
             }
@@ -59,6 +58,8 @@ double Solver::optimise_path(vector<Point> path) {
     return path_dist(path);
 }
 
+
+
 void Solver::do2opt(vector<Point> &path, int i, int j) {
     swap(*(path.begin() + i + 1), *(path.begin() + j + 1));
 }
@@ -66,9 +67,9 @@ void Solver::do2opt(vector<Point> &path, int i, int j) {
 double Solver::path_dist(vector<Point> &path) {
     double distance = 0;
     for(int i = 0; i < path.size() - 1; i++){
-        distance += path[i].dist(path[i + 1]);
+        distance += dist(path[i], path[i + 1]);
     }
-    distance += path[path.size() - 1].dist(path[0]);
+    distance += dist(path[path.size() - 1], path[0]);
     return distance;
 }
 
@@ -79,8 +80,8 @@ int main(){
     Solver solver;
     for(int i = 0; i < n; i++)
         cin >> path[i].x >> path[i].y;
+    cout << solver.path_dist(path)<<endl;
     cout << solver.optimise_path(path) << endl;
-    for(int i = 0; i < n; i++)
-        cout << path[i].x <<" "<< path[i].y<<endl;
     return 0;
 }
+
